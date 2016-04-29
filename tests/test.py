c3e576863d748a4c88baad3d41d65ac8472773df
Joshua Downer <joshua.downer@gmail.com>
2016-04-29 05:54:48 -0400

    added more unit tests

--- tests/test.py
+++ tests/test.py
@@ -60,3 +60,77 @@ class TestRequiresProvides(unittest.TestCase):
 
         with self.assertRaises(dependency.MissingRequirementError):
             test_func()
+
+    def test_multiple_requirements(self):
+        @dependency.provides('test-req-A')
+        class TestClassA(object):
+            pass
+
+        @dependency.provides('test-req-B')
+        class TestClassB(object):
+            pass
+
+        @dependency.requires('test-req-A', 'test-req-B')
+        class TestClassB(object):
+            pass
+
+        @dependency.provides('test-req-func-A')
+        def funcA():
+            pass
+
+        @dependency.provides('test-req-func-B')
+        def funcB():
+            pass
+
+        @dependency.requires('test-req-func-A', 'test-req-func-B')
+        def funcC():
+            pass
+
+    def test_mixed_requirements(self):
+        @dependency.provides('test-req-A')
+        def funcA():
+            pass
+
+        @dependency.provides('test-req-B')
+        @dependency.requires('test-req-A')
+        def funcB():
+            pass
+
+        @dependency.requires('test-req-A', 'test-req-B')
+        def funcC():
+            pass
+
+        @dependency.requires('test-req-A', 'test-req-B')
+        @dependency.requires('test-req-C', dynamic=True)
+        def funcD():
+            pass
+
+        with self.assertRaises(dependency.MissingRequirementError):
+            funcD()
+
+        with dependency.provides('test-req-C'):
+            funcD()
+
+    def test_multiple_provision(self):
+        @dependency.provides('test-req-A')
+        def funcA():
+            pass
+
+        with self.assertRaises(dependency.MissingRequirementError):
+            @dependency.requires('test-req-A', 'test-req-B')
+            def funcB():
+                pass
+
+        with dependency.provides('test-req-A', 'test-req-B'):
+            @dependency.requires('test-req-A', 'test-req-B')
+            def funcC():
+                pass
+
+        @dependency.requires('test-req-A')
+        def funcD():
+            pass
+
+        with self.assertRaises(dependency.MissingRequirementError):
+            @dependency.requires('test-req-B')
+            def funcE():
+                pass
